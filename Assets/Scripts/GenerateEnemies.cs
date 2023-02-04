ede3ff45694e484250fc5f72bb6875a9bd2a50c9
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-02-03 21:53:34 -0500

    wave spawning didnt work

--- Assets/Scripts/GenerateEnemies.cs
+++ Assets/Scripts/GenerateEnemies.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class GenerateEnemies : MonoBehaviour
 {
-    [System.Serializable]
-    public class waveContent
-    {
-        [SerializeField] GameObject[] EnemySpawner;
-
-        public GameObject[] GetEnemySpawnerList()
-        {
-            return EnemySpawner;
-        }
-    }
-    [SerializeField] [NonReorderable] waveContent[] waves;
-    int currentWave = 0;
-
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +11,5 @@ public class GenerateEnemies : MonoBehaviour
 
 
     }
-    void SpawnWave()
-    {
-        for(int i = 0; i < waves[currentWave].GetEnemySpawnerList().Length; i++)
-        {
-            Vector3 SpawnPos;
-            float xPos = Random.Range(13, 16) + transform.position.x;
-            float zPos = Random.Range(-8, 9) + transform.position.z;
-            Instantiate(waves[currentWave].GetEnemySpawnerList()[i]);
 
-            SpawnPos = new Vector3(xPos, 0, zPos);
-        }
-    }
 }

285df306a6784a96832facb3afaf620b9a9a57c3
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-02-06 21:27:39 -0500

    player shoot and destroys enemy

---
+++ Assets/Scripts/TumbleWeed.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleWeed : MonoBehaviour
+{
+    public float speed;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(Vector3.left * speed * Time.deltaTime);
+        //controller.Rotate;
+    }
+}
